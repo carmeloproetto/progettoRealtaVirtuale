249d90ab91c60dc23dc78f0ece18cca778414808
lauc98 <lauracossu988@gmail.com>
2022-02-07 18:47:07 +0100

    tasto M

---
+++ Assets/Scripts/PressM.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressM : MonoBehaviour
+{
+    public GameObject MUI;
+
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+    public Transform teleportTarget2;
+    public GameObject thePlayer2;
+
+    void Start()
+    {
+        MUI = transform.GetChild(0).gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            PauseGame();
+        }
+        
+    }
+
+    public void PauseGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        MUI.SetActive(true);
+        Time.timeScale = 0f;
+        AudioListener.pause = true;
+    }
+
+    public void ResumeGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        MUI.SetActive(false);
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        FindObjectOfType<AudioManager>().Play("BottoneAscensore");
+    }
+
+    public void OnTriggerEnter()
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;
+        thePlayer2.transform.position = teleportTarget2.transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
+        MUI.SetActive(false);
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        FindObjectOfType<AudioManager>().Play("BottoneAscensore");
+    }
+
+   
+}
