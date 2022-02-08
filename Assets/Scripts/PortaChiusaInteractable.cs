5c328a8b9485471761d18f5a79b2388812ca6540
Agazio <agazio98@live.it>
2022-02-08 11:32:22 +0100

    Collider Porte Modificato

---
+++ Assets/Scripts/PortaChiusaInteractable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortaChiusaInteractable : Interactable
+{
+    private Animator _animator;
+
+    public override void Interact(GameObject caller)
+    {
+        _animator.SetTrigger("Apri"); 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+}
