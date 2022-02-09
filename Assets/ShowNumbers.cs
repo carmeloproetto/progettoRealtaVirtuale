a3cc2aeac23f6421b66db980eec064f801bd5049
DESKTOP-A1EMRJQ\carlo <carlovitale99@live.it>
2022-02-09 21:01:28 +0100

    risonanza1

---
+++ Assets/ShowNumbers.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowNumbers : MonoBehaviour
+{
+    public List<string> numberSequence;
+    public bool correct;
+    public int levelSize;
+    public int level;
+    public List<string> correctSequence;
+
+    TMPro.TextMeshProUGUI display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        display = gameObject.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>();
+        numberSequence = new List<string>();
+        correct = false;
+        levelSize = 5;
+        level = 1;
+
+        
+        for (int i = 1; i <= levelSize; i++)
+        {
+            correctSequence.Add(i.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int c = 0;
+        string concat = "";
+        numberSequence.ForEach(x => { concat += x; });  //Debug.Log(c + "-" + x); c++; });
+        if(numberSequence.Count != 0)
+            display.SetText(concat);
+        c = 0;
+    }
+
+    public void DisplayMessage(string msg)
+    {
+        display.SetText(msg);
+    }
+
+
+}
