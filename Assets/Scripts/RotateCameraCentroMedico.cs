36ee717c5adf31e7d68a63a835256c45b1918241
carmeloproetto <carmeloproetto7@gmail.com>
2022-02-24 01:05:56 +0100

    Aggiustata camera risonanza

--- Assets/Scripts/RotateCameraCentroMedico.cs
+++ Assets/Scripts/RotateCameraCentroMedico.cs
@@ -23,9 +23,9 @@ public class RotateCameraCentroMedico : MonoBehaviour
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
 
-        yaw = Mathf.Clamp(yaw, 30f, 120f);
+        yaw = Mathf.Clamp(yaw, 70f, 120f);
         //the rotation range
-        pitch = Mathf.Clamp(pitch, -60f, 10f);
+        pitch = Mathf.Clamp(pitch, -40f, 10f);
         //the rotation range
 
         transform.localEulerAngles = new Vector3(pitch, yaw, 0.0f);
